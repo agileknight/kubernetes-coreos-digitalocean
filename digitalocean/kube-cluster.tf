ea01c0df08319a250d44388cf74557c447ce407c
Philipp Meier <philipp.meier@codecentric.de>
2015-12-09 00:32:37 +0100

    pass user data and extend to 3 servers now tested successfully

--- digitalocean/kube-cluster.tf
+++ digitalocean/kube-cluster.tf
@@ -21,8 +21,29 @@ resource "digitalocean_droplet" "kube-master" {
     size = "512mb"
     private_networking = true
     ssh_keys = ["${var.do_ssh_key_id}"]
+    user_data = "${template_file.do_cloud_config.rendered}"
 }
 
-output "foo" {
+resource "digitalocean_droplet" "kube-slave01" {
+    image = "coreos-beta"
+    name = "kube-slave01"
+    region = "fra1"
+    size = "512mb"
+    private_networking = true
+    ssh_keys = ["${var.do_ssh_key_id}"]
+    user_data = "${template_file.do_cloud_config.rendered}"
+}
+
+resource "digitalocean_droplet" "kube-slave02" {
+    image = "coreos-beta"
+    name = "kube-slave02"
+    region = "fra1"
+    size = "512mb"
+    private_networking = true
+    ssh_keys = ["${var.do_ssh_key_id}"]
+    user_data = "${template_file.do_cloud_config.rendered}"
+}
+
+output "do_etcd_discovery_url" {
     value = "${etcdiscovery_token.do_cluster.id}"
 }
